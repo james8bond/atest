b5517079655680389857f0f62f80c0ce988f2dfd
james8bond <15974708423@139.com>
2021-03-13 21:50:46 +0800

    添加 请求类 Http 和 配置文件config

---
+++ config/config.js
@@ -0,0 +1,6 @@
+const config = {
+  // 配置小程序的域名
+  apiBaseUrl: 'http://rurujia.top/'
+}
+
+export { config }
---
+++ utils/http.js
@@ -0,0 +1,18 @@
+import { config } from "../config/config";
+import { promisic } from "./util";
+
+class Http {
+  static async request({ url, data = {}, method = 'GET' }) {
+    let res = await promisic(wx.request)({
+      url: `${config.apiBaseUrl}${url}`,
+      data: data,
+      method: method,
+      header: {
+        token: wx.getStorageSync('token'),
+      },
+    });
+    return res.data; // res 是整个包, 包括 cookie, header, body => data, 应该把body返回, 也就是 res.data
+  }
+}
+
+export { Http }
